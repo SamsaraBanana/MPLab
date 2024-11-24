3ee9c5b23b90d94d12c7f03b838d7eaf14588755
Lukas Ridler <lukas.rid1@hotmail.de>
2024-11-24 18:47:08 +0100

    Init for livecode1

---
+++ app/src/main/java/com/example/mplab_todoapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mplab_todoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.mplab_todoapp.ui.screens.TodoListScreen
+import com.example.mplab_todoapp.ui.theme.MPLab_ToDoAppTheme
+import com.example.mplab_todoapp.viewmodel.TodoViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MPLab_ToDoAppTheme {
+                Scaffold(
+                    content = { paddingValues ->
+                        TodoListScreen(viewModel = TodoViewModel(), modifier = Modifier.padding(paddingValues))
+                    }
+                )
+            }
+        }
+    }
+}
